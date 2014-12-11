736df5106778321108690c6d1e2f86a8cfe2792d
peterskipper <peter.skipper@gmail.com>
2014-12-11 15:51:08 -0800

    Travis CI

--- tests/infection_graph_tests.py
+++ tests/infection_graph_tests.py
@@ -28,9 +28,23 @@ class TestInfectionGraph(unittest.TestCase):
         enid = 5
         self.assertRaises(AttributeError, self.graph.add_connection(
             self.alice, enid))
+        
+
+    def testRemoveEdge(self):
+        self.graph.remove_connection(self.alice, self.bob)
+        self.assertEqual(self.graph.degree(0), 1)
+        self.assertEqual(self.graph.degree(1), 0)
+        self.assertEqual(self.graph.degree(2), 1)
+        self.assertEqual(self.graph.degree(3), 0)
+        links = [sub for sub in nx.connected_components(self.graph)]
+        self.assertEqual(len(links), 3)
+        self.assertTrue([0,2] in links)
+        self.assertTrue([1] in links)
+        self.assertTrue([3] in links)
+        enid = 5
         self.assertRaises(AttributeError, self.graph.remove_connection(
             self.bob, enid))
-        
+
     @classmethod
     def tearDownClass(self):
         self.graph.clear()
